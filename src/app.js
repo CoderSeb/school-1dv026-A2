7b6c5688b0010e535b0ac9775ff438c76e40de16
Sebastian <sa224ny@student.lnu.se>
2020-12-23 14:04:20 +0100

    Calendar scraping done, stuck with promise problems, merry x-mas to me

--- src/app.js
+++ src/app.js
@@ -1,19 +1,10 @@
-import axios from 'axios'
-import cheerio from 'cheerio'
-import mainScraper from './components/main-scraper.js'
+import mainScraper from './javascripts/main-scraper.js'
 
 const url1 = 'https://cscloud6-127.lnu.se/scraper-site-1'
 const url2 = 'https://cscloud6-127.lnu.se/scraper-site-2'
 
-const startScraping = async () => {
-  const response = await axios.get(url1)
-  return cheerio.load(response.data)
-}
+const [,, path] = process.argv
 
-const scrapedData = await startScraping()
-const links = new Set()
-scrapedData('a').each((i, link) => {
-  links.add(scrapedData(link).attr('href'))
-})
+console.log(`Web scraping ${path}\nThis may take some time...`)
 
-mainScraper(links)
+mainScraper(url1)
