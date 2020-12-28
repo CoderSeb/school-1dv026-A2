e1ff69c59fb42f31aff8aadbef4c93b788e56e2d
Sebastian <sa224ny@student.lnu.se>
2020-12-28 12:46:05 +0100

    still stuck...

--- src/javascripts/main-scraper.js
+++ src/javascripts/main-scraper.js
@@ -1,85 +1,60 @@
 import axios from 'axios'
 import cheerio from 'cheerio'
 
+const fullUsers = []
+let userObj = {}
+const mainLinks = []
+const calendarLinks = []
+
 /**
  * @param path
  */
-const mainScraper = async (path) => {
-  const scrapedData = await startScraping(path)
-  // Creating a new Set to prevent duplicates.
-  const links = new Set()
-
-  let fullPeopleObject = []
-  // Going through each a-tag to get the href links and adding them to links set.
-  scrapedData('a').each((i, link) => {
-    links.add(scrapedData(link).attr('href'))
-  })
-  // Creating an array from the links set.
-  const urls = Array.from(links)
-  // Looping through the links.
-  urls.forEach(url => {
-    if (url.includes('calendar')) {
-      const people = []
-      /**
-       *
-       */
-      const scrapeCalendar = async () => {
-        const scrapedCalendar = await startScraping(url)
-        scrapedCalendar('a').each((i, link) => {
-          const person = {
-            name: scrapedCalendar(link).text().toUpperCase(),
-            calendarUrl: `${url}${scrapedCalendar(link).attr('href').substring(2)}`
+function mainScraper (path) {
+  axios.get(path).then(response => {
+    const $ = cheerio.load(response.data)
+    $('a').each((index, item) => {
+      mainLinks.push($(item).attr('href'))
+    })
+    return mainLinks
+  }).then(mainLinks => {
+    mainLinks.forEach(link => {
+      // If link contains calendar path
+      if (link.includes('calendar')) {
+        axios.get(link).then(response => {
+          const $ = cheerio.load(response.data)
+          $('a').each((index, item) => {
+            calendarLinks.push($(item).attr('href').substring(2))
+          })
+          return calendarLinks
+        }).then(calendarLinks => {
+          console.log(calendarLinks)
+          console.log(mainLinks)
+          for (const link of calendarLinks) {
+            axios.get(mainLinks[0] + link).then(response => {
+              const $ = cheerio.load(response.data)
+              const weekDays = []
+              $('tbody > tr > td').each((index, item) => {
+                weekDays.push($(item).text().toUpperCase())
+              })
+                userObj = {
+                  name: $('h2').text(),
+                  calendarUrl: mainLinks[0] + link,
+                  availableDays: {
+                    friday: weekDays[0].includes('OK'),
+                    saturday: weekDays[1].includes('OK'),
+                    sunday: weekDays[2].includes('OK')
+                  }
+                }
+                fullUsers.push(userObj)
+                if (fullUsers.length === 3) {
+                  return fullUsers
+                }
+              })
           }
-          people.push(person)
         })
-        return people
       }
-
-      fullPeopleObject = scrapeCalendar()
-        .then(persons => {
-        scrapeDays(persons).then(result => {
-          return result
-        })
-      })
-
-      console.log(fullPeopleObject)
-    }
+    })
   })
-}
-
-/**
- * @param persons
- */
-const scrapeDays = async (persons) => {
-  const result = []
-  for (const person of persons) {
-    const scrapedPersonCal = await startScraping(person.calendarUrl)
-    const individualWeekends = scrapedPersonCal('tbody > tr > td').text().toUpperCase()
-    const friday = individualWeekends.substring(0, 2).includes('OK')
-    const saturday = individualWeekends.substring(2, 4).includes('OK')
-    const sunday = individualWeekends.substring(4, 6).includes('OK')
-    const personObj = {
-      name: person.name,
-      calUrl: person.calendarUrl,
-      availableWeekend: {
-        friday: friday,
-        saturday: saturday,
-        sunday: sunday
-      }
-    }
-    result.push(personObj)
-  }
-  return result
-}
-
-/**
- * Function that takes in a url and returns the data from that url.
- *
- * @param {string} path as the url.
- */
-const startScraping = async (path) => {
-  const response = await axios.get(path)
-  return cheerio.load(response.data)
 }
 
 export default mainScraper
