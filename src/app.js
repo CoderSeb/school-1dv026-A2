e91e865cfda19a8ece6a4e10b1fdfb40ee46f7b4
Sebastian <sa224ny@student.lnu.se>
2021-01-05 17:12:01 +0100

    Added url validator and README to showcase the npm packages used

--- src/app.js
+++ src/app.js
@@ -6,16 +6,16 @@
  */
 
 // Imports
+import validator from 'validator'
 import mainScraper from './javascripts/main-scraper.js'
 
 const [,, path] = process.argv
 
 try {
-  if (!path) {
-    console.log('Web scraping https://cscloud6-127.lnu.se/scraper-site-1\nThis may take some time...\n')
-    mainScraper('https://cscloud6-127.lnu.se/scraper-site-1')
+  if (!path || !validator.isURL(path)) {
+    console.log('Hi there!\nThis is a web scraper and therefore it needs a url, please enter one after the start script to begin!')
   } else {
-    console.log(`Web scraping ${path}\nThis may take some time...\n`)
+    console.log(`Scraping ${path}\nThis may take some time...\n`)
     mainScraper(path)
   }
 } catch (err) {
